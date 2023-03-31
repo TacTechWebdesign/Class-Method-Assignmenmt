7b1c0939e3c1c6f2a893b8287956d764846bbdd1
TacTechWebdesign <118140620+TacTechWebdesign@users.noreply.github.com>
2023-03-31 23:41:06 +0800

    Add project files.

---
+++ Class Method Assignmenmt/Method.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Assignmenmt
+{
+    class Method
+    {
+        //Create a void method that takes two integers as parameters
+        public void functionOne(int int1, int int2)
+
+        {
+            int Total = int1 + 2;
+            Console.WriteLine(int2);
+        }
+    }
+}
---
+++ Class Method Assignmenmt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Assignmenmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 16;
+            int num2 = 24;
+            Method function = new Method();
+
+            function.functionOne(num1, num2);
+
+            function.functionOne(int1: num1, int2: num2);
+
+            Console.ReadLine();
+        }
+    }
+}
